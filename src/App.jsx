7f45a163353df08d16863bbbc8fb5eed7b3c5d9a
Colby <maquiret@gmail.com>
2025-10-30 14:10:24 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p className="bg-amber-500 h-full text-4xl">Hello Begin inventory manager proj</p>
+    </>
+  )
+}
+
+export default App
