5f2ca7c2283fc566aa87381d153faf66e3d24161
Colby <maquiret@gmail.com>
2025-10-30 20:12:49 -0500

    mess with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-
+import { Routes } from react-router
+import Banner from './assets/banner'
 
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <p className="bg-amber-500 h-full text-4xl">Hello Begin inventory manager proj</p>
+    <Routes>
+      <Banner />
+    </Routes>
     </>
   )
 }
---
+++ src/assets/banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <>
+        <div classname="flex w-screen items-center justify-center">
+            <h1 >Inventory Pro</h1>
+        </div>
+    </>
+  )
+}
+
+export default Banner
